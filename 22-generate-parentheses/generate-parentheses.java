ca568d3c9a4303e00df987df037a7da4e8fe91a4
Om Misal <omsmisal@gmail.com>
2026-07-05 17:51:38 +0530

    Time: 2 ms (71.88%) | Memory: 44.7 MB (45.57%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    List<String> ans = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        generate("", n, n);
+        return ans;
+    }
+
+    private void generate(String str, int open, int close) {
+
+        if (open == 0 && close == 0) {
+            ans.add(str);
+            return;
+        }
+
+        if (open > 0) {
+            generate(str + "(", open - 1, close);
+        }
+
+        if (close > open) {
+            generate(str + ")", open, close - 1);
+        }
+    }
+}
