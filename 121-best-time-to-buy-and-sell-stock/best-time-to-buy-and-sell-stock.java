a0784a83ab881dc87216009a9be73c49e85a9200
Om Misal <omsmisal@gmail.com>
2026-07-18 17:58:54 +0530

    Time: 1 ms (99.94%) | Memory: 94.6 MB (33.22%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max = 0;
+        int left = 0, right = 1;
+
+        while(right < n) {
+            if(prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                max = Math.max(max, profit);
+            }
+            else {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+}
